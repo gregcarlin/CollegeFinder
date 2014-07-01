7f6d76f51ab8debc87bcca5f2d2af91005e30987
Greg Carlin <gregthegeek@optonline.net>
2014-07-01 14:54:38 -0400

    More info stuff

--- info.php
+++ info.php
@@ -3,24 +3,38 @@
   $extra = '<link href="styles/info.css" rel="stylesheet" />';
   require_once "util/header-signedin.php";
 
+  function n($name) {
+    if(isset($_POST['name']) && strlen($_POST['name']) > 0) {
+      return $_POST['name'];
+    } else {
+      return NULL;
+    }
+  }
+
+  $changes = false;
+  $error = 0;
+  if(anySet('fname', 'lname', /*'email',*/ 'sat', 'satMT', 'satCR', 'satWR', 'act', 'actEN', 'actMT', 'actRD', 'actSC', 'actWR', 'gpaWeight', 'gpaNoWeight')) {
+    $fname = n('fname');
+    $lname = n('lname');
+    //$email = n('email');
+    if($fname == NULL || $lname == NULL) {
+      $error = 1;
+    }/* else if($email == NULL) {
+      $error = 2;
+    } else {*/
+
+      $stmt = $mysql->prepare("UPDATE `students` SET `fname` = ?, `lname` = ?, `email` = ?, `sat` = ?, `sat_mt` = ?, `sat_cr` = ?, `sat_wr` = ?, `act` = ?, `act_en` = ?, `act_mt` = ?, `act_rd` = ?, `act_sc` = ?, `act_wr` = ?, `gpa_weight` = ? WHERE `id` = ?");
+      $stmt->bind_param('sssssssssssssssi', $fname, $lname, $email, n('sat'), n('satMT'), n('satCR'), n('satWR'), n('act'), n('actEN'), n('actMT'), n('actRD'), n('actSC'), n('actWR'), n('gpaWeight'), n('gpaNoWeight'), $id);
+      $stmt->execute();
+      $stmt->close();
+      $changes = true;
+
+    }
+  }
+
   $stmt = $mysql->prepare("SELECT `fname`,`lname`,`email`,`sat`,`sat_mt`,`sat_cr`,`sat_wr`,`act`,`act_en`,`act_mt`,`act_rd`,`act_sc`,`act_wr`,`gpa_weight`,`gpa_noweight` FROM `students` WHERE `id` = ?");
   $stmt->bind_param("i", $id);
   $stmt->execute();
-  /*$fname = NULL;
-  $lname = NULL;
-  $email = NULL;
-  $sat = NULL;
-  $satMT = NULL;
-  $satCR = NULL;
-  $satWR = NULL;
-  $act = NULL;
-  $actEN = NULL;
-  $actMT = NULL;
-  $actRD = NULL;
-  $actSC = NULL;
-  $actWR = NULL;
-  $gpaWeight = NULL;
-  $gpaNoWeight = NULL;*/
   $vars = array(
       "fname" => NULL,
       "lname" => NULL,
@@ -38,16 +52,27 @@
       "gpaWeight" => NULL,
       "gpaNoWeight" => NULL,
     );
-  //$stmt->bind_result($fname, $lname, $email, $sat, $satMT, $sat);
   $stmt->bind_result($vars["fname"], $vars["lname"], $vars["email"], $vars["sat"], $vars["satMT"], $vars["satCR"], $vars["satWR"], $vars["act"], $vars["actEN"], $vars["actMT"], $vars["actRD"], $vars["actSC"], $vars["actWR"], $vars["gpaWeight"], $vars["gpaNoWeight"]);
   assert($stmt->fetch());
-  //var_dump($vars);
+  $stmt->close();
 
   function a($name, $title) {
     global $vars;
     echo '<tr>';
     echo '<td><label for="' . $name . '">' . $title . '</label></td>';
-    echo '<td><input type="text" class="form-control" id="' . $name . '" name="' . $name . '" value="' . $vars[$name] . '" /></td>';
+    echo '<td><input type="text" class="form-control" id="' . $name . '" name="' . $name . '" value="' . (isset($_POST[$name]) ? $_POST[$name] : $vars[$name]) . '" /></td>';
+    echo '</tr>';
+  }
+
+  function blank() {
+    echo '<tr>';
+    echo '<td colspan="2" class="blank"></td>';
+    echo '</tr>';
+  }
+
+  function doubleBlank() {
+    echo '<tr>';
+    echo '<td colspan="2" class="double-blank"><span></span></td>';
     echo '</tr>';
   }
 ?>
@@ -57,34 +82,43 @@
       <div class="starter-template">
         <h1>Update your information</h1>
 
-        <table>
-          <!--<tr>
-            <td><label for="fname">First Name:</label></td>
-            <td><input type="text" class="form-control" id="fname" name="fname" /></td>
-          </tr>
-          <tr>
-            <td><label for="lname">Last Name:</label></td>
-            <td><input type="text" class="form-control" id="lname" name="lname" /></td>
-          </tr>
-          <tr>
-            <td><label for=""></label></td>
-            <td><input type="text" class="form-control" id="" name="" /></td>
-          </tr>
-          <tr>
-            <td><label for=""></label></td>
-            <td><input type="text" class="form-control" id="" name="" /></td>
-          </tr>-->
-          <?php
-            a("fname", "First Name");
-            a("lname", "Last Name");
-            a("email", "Email Address");
-            a("sat", "Best Single-Sitting SAT");
-            a("satMT", "Best SAT Math");
-            a("satCR", "Best SAT Reading");
-            a("satWR", "Best SAT Writing");
-            a("act", "Best Single-Sitting ACT");
-          ?>
-        </table>
+        <form action="#" method="post">
+          <table>
+            <?php
+
+              a("fname", "First Name");
+              a("lname", "Last Name");
+              /*doubleBlank();
+              //if($error == 2) {
+                echo '<tr>';
+                echo '<td colspan="2" class="error">You must have a valid email address.</td>';
+                echo '</tr>';
+              //}
+              a("email", "Email Address");*/
+              doubleBlank();
+              a("sat", "Best Single-Sitting SAT");
+              blank();
+              a("satMT", "Best SAT Math");
+              a("satCR", "Best SAT Reading");
+              a("satWR", "Best SAT Writing");
+              doubleBlank();
+              a("act", "Best Single-Sitting ACT");
+              blank();
+              a("actEN", "Best ACT English");
+              a("actMT", "Best ACT Math");
+              a("actRD", "Best ACT Reading");
+              a("actSC", "Best ACT Science");
+              a("actWR", "Best ACT Writing");
+              doubleBlank();
+              a("gpaWeight", "Unweighted GPA");
+              a("gpaNoWeight", "Weighted GPA");
+              doubleBlank();
+            ?>
+            <tr>
+              <td colspan="2"><input type="submit" class="btn btn-primary" value="Save Changes" /></td>
+            </tr>
+          </table>
+        </form>
       </div>
 
     </div>
